dc62ea6ca8fdfbe30ad356d5c94389131c4f3021
Priyank Shrivastava <priyaank.shrivastava@gmail.com>
2021-02-19 01:51:39 +0530

    User Registration

--- config/db.js
+++ config/db.js
@@ -8,7 +8,8 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useCreateIndex: true,
+      useUnifiedTopology: true,
     });
 
     console.log("MongoDB Connected...");
