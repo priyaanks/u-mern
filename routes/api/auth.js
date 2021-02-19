c29974167456c1d61607a47ca603fdcc6fa9198d
Priyank Shrivastava <priyaank.shrivastava@gmail.com>
2021-02-19 18:05:28 +0530

    UAnthentication and Login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,12 @@
 const express = require("express");
-const router = express.Router();
 const auth = require("../../middleware/auth");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const bcrypt = require("bcryptjs");
+
+const router = express.Router();
+
+const { check, validationResult } = require("express-validator");
 
 // @route     GET api/auth
 // @desc      Test route
@@ -14,5 +20,66 @@ router.get("/", auth, async (req, res) => {
     res.status(500).send("Server error");
   }
 });
+
+const User = require("../../modals/User");
+// @route     POST api/auth
+// @desc      Authenticate User & get token
+// @access    Public
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // Check if the user enters valid email
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      // Check if password matches
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      // if password doesn't matches
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
