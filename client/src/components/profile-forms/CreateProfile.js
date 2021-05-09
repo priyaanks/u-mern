2a1ec0e95f85e3414034004ab4aa9872a06cdfba
Priyank Shrivastava <priyaank.shrivastava@gmail.com>
2021-05-10 02:25:10 +0530

    Create Profile Action

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -16,7 +18,7 @@ const CreateProfile = props => {
     facebook: '',
     linkedin: '',
     instagram: '',
-  })
+  });
 
   const [displaySocialInputs, toggleSocialInputs] = useState(false);
 
@@ -40,6 +42,11 @@ const CreateProfile = props => {
     [e.target.name]: e.target.value
   })
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
+
   return (
     <>
      <h1 className="large text-primary">
@@ -50,7 +57,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={ status } onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -141,7 +148,7 @@ const CreateProfile = props => {
 
             <div className="form-group social-input">
               <i className="fab fa-instagram fa-2x"></i>
-              <input type="text" placeholder="Instagram URL" name="instagram" value={ status } onChange={e => onChange(e)} />
+              <input type="text" placeholder="Instagram URL" name="instagram" value={ instagram } onChange={e => onChange(e)} />
             </div>
           </>
         )}
@@ -155,7 +162,8 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
-
+  createProfile: PropTypes.func.isRequired,
 }
 
-export default CreateProfile
+
+export default connect(null, { createProfile })(withRouter(CreateProfile))
