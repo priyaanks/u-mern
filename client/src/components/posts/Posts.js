b42ee0fc04dd29e4127e0aa08e203fc7d9eb95f2
Priyank Shrivastava <priyaank.shrivastava@gmail.com>
2021-05-11 03:19:08 +0530

    PostItem Component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,14 +2,30 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Spinner } from "../layout/Spinner";
+import PostItem from "./PostItem";
 import { getPosts } from "../../actions/post";
 
-const Posts = ({ getPosts, post }) => {
+const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return <></>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Welcome to the community
+      </p>
+      {/* PostForm */}
+      <div className='posts'>
+        {posts.map((post) => {
+          return <PostItem key={post._id} post={post} />;
+        })}
+      </div>
+    </>
+  );
 };
 
 Posts.propTypes = {
@@ -18,7 +34,7 @@ Posts.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
-  posts: state.post,
+  post: state.post,
 });
 
 export default connect(mapStateToProps, { getPosts })(Posts);
