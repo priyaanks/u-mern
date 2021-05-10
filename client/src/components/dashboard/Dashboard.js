d64b309eb249e6536c6a2e033ce13511e77ddcbe
Priyank Shrivastava <priyaank.shrivastava@gmail.com>
2021-05-11 02:22:18 +0530

    Addressing Console Warning

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -17,8 +17,7 @@ const Dashboard = ({
 }) => {
   useEffect(() => {
     getCurrentProfile();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [getCurrentProfile]);
 
   return loading && profile === null ? (
     <Spinner />
