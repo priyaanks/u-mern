22b01f1b6391827708338af5bbae82a8c7c6ee74
Priyank Shrivastava <priyaank.shrivastava@gmail.com>
2021-05-11 21:27:27 +0530

    Prepared for Deployment

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use("/api/posts", require("./routes/api/posts"));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  // set static folder
+  // Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
